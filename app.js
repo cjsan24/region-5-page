d970adb1dd8c451dca4b531af154e5e2364cac59
CJ Sanchez <75692144+cjsan24@users.noreply.github.com>
2023-07-25 16:51:36 -0700

    Added in some Javascript responsiveness

---
+++ app.js
@@ -0,0 +1,20 @@
+const modal = document.getElementById('modal');
+const submitBtn = document.getElementById('submit-button');
+const closeBtn = document.getElementsByClassName('close')[0];
+
+
+submitBtn.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  modal.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', function() {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
